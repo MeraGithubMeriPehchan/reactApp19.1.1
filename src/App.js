55c56046fd2384a639cce723fdf545a25bbc2ba8
mohd shamim <mhdshamim1@gmail.com>
2025-09-03 16:48:29 +0000

    added confirm delete for model pop up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import Modal from "./components/Modal";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [todoToDelete, setTodoToDelete] = useState(null);
 
   const addTodo = (text) => {
     setTodos([...todos, { text, completed: false }]);
   };
 
-  const deleteTodo = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
+  const confirmDelete = (index) => {
+    setTodoToDelete(index); // store which todo is to be deleted
+    setModalOpen(true);
+  };
+
+  const deleteTodo = () => {
+    if (todoToDelete !== null) {
+      setTodos(todos.filter((_, i) => i !== todoToDelete));
+      setTodoToDelete(null);
+      setModalOpen(false);
+    }
   };
 
   const toggleTodo = (index) => {
@@ -22,9 +34,17 @@ function App() {
 
   return (
     <div>
-      <h1>Todo App (Modular + Complete)</h1>
+      <h1>Todo App (with Delete Confirmation Modal)</h1>
       <TodoInput onAdd={addTodo} />
-      <TodoList todos={todos} onDelete={deleteTodo} onToggle={toggleTodo} />
+      <TodoList todos={todos} onDelete={confirmDelete} onToggle={toggleTodo} />
+
+      {modalOpen && (
+        <Modal onClose={() => setModalOpen(false)}>
+          <h2>Are you sure you want to delete this task?</h2>
+          <button onClick={deleteTodo}>Yes, Delete</button>
+          <button onClick={() => setModalOpen(false)}>Cancel</button>
+        </Modal>
+      )}
     </div>
   );
 }
